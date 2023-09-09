af2f68ab4b8fe4adfc17d0bfa4006b15cb4aab46
Mario Cruz <mario@mariocruz.net>
2023-09-09 17:20:00 -0400

    Update PoemAI.py
    Added voice feedback for user selections and added poem attribution after reading.
    
    - Introduced voice feedback to read out user's poem choices.

--- PoemAI.py
+++ PoemAI.py
@@ -9,8 +9,11 @@ def clear_screen():
     """Clear the terminal screen based on the operating system."""
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def speak_text(text):
-    """Make the Mac read out the provided text using a male voice."""
+def speak_text(text, pause_duration=0):
+    """Make the Mac read out the provided text using a male voice. Includes an optional pause."""
+    if pause_duration > 0:
+        pause_cmd = f"[[slnc {pause_duration * 1000}]]"  # slnc command in say requires milliseconds
+        text = text + pause_cmd
     os.system(f'say -v Samantha "{text}"')
 
 def get_user_choice(options, prompt):
@@ -29,15 +32,20 @@ def get_user_choice(options, prompt):
         except ValueError:
             print("Please enter a valid number.")
 
+def speak_user_choices(color, location, feeling, wildcard, poem_style):
+    """Speak out the user's choices using the Mac's voice feature."""
+    choice_text = (f"You chose the color {color}, location {location}, feeling {feeling}, "
+                   f"wildcard {wildcard}, and the poem style {poem_style}.")
+    speak_text(choice_text)
+
 def generate_poem_with_openai():
-    """Generate a poem based on user's choices using OpenAI."""
     colors = ["Red", "yellow", "Blue", "orange", "green"]
     locations = ["Miami Beach", "Downtown Miami", "Wynwood Miami", "underline Miami", "Little Havana, Miami"]
     feelings = ["Happy", "Anxious", "nostalgic", "frustrated", "romantic"]
     wildcards = ["Mojito", "music", "magic", "epic", "books"]
     poem_styles = ["Sonnet", "Haiku", "Free Verse", "Limerick", "Villanelle"]
 
-    while True:  # Start a loop for confirmation 
+    while True:
         color_choice = get_user_choice(colors, "Choose a color:")
         location_choice = get_user_choice(locations, "Choose a location:")
         feeling_choice = get_user_choice(feelings, "Choose a feeling:")
@@ -55,7 +63,9 @@ def generate_poem_with_openai():
         print(f"Wildcard: {wildcard_choice}")
         print(f"Poem Style: {poem_style_choice}\n")
 
-        # Ask user for confirmation
+        # Speak the user's choices
+        speak_user_choices(color_choice, location_choice, feeling_choice, wildcard_choice, poem_style_choice)
+
         proceed = input("Do you want to proceed with these choices? (yes/no): ").strip().lower()
 
         if proceed == 'yes':
@@ -67,29 +77,24 @@ def generate_poem_with_openai():
             print("Invalid input. Please answer with 'yes' or 'no'.\n")
             continue
 
-    # Check if the API key is set
     if not openai.api_key:
         return "Error: OpenAI API key not set. Please set the 'OPENAI_API_KEY' environment variable."
 
-    # Create the prompt
     prompt = (f"Write a {poem_style_choice} about {location_choice} with themes of {color_choice}, "
              f"{feeling_choice}, and {wildcard_choice}.")
 
-    # Number of retries
-    retries = 3  # Adjust as needed
-
+    retries = 3
     for attempt in range(retries):
         try:
             response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "system", "content": "You are a helpful assistant."},{"role": "user", "content": prompt}])
-
-            if 'choices' in response and len(response['choices']) > 0 and 'message' in response['choices'][0] and 'content' in response['choices'][0]['message']:
-                return response['choices'][0]['message']['content'].strip()
+            content = response.get('choices', [{}])[0].get('message', {}).get('content')
+            if content:
+                return content.strip()
             else:
                 return "Error: The response from OpenAI was unexpected."
-
         except openai.error.OpenAIError as e:
-            if attempt < retries - 1:  # Check if it's the last retry
-                time.sleep(1)  # Wait for 1 second before retrying
+            if attempt < retries - 1:
+                time.sleep(1)
                 continue
             else:
                 return f"OpenAI Error after {retries} attempts: {e}"
@@ -102,7 +107,8 @@ clear_screen()
 # Display the Poem
 print("Poem-O-Matic-AI by MarioTheMaker\n")
 poem = generate_poem_with_openai()
-
 print(poem + "\n")
 speak_text(poem)
 
+attribution = "Poem Created by Poem-O-Matic-AI by Mario The Maker for O'Miami festival."
+speak_text(attribution, pause_duration=2)
