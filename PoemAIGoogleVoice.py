40e350d72b182c726f27118e2ac7441a989b42ba
Mario Cruz <mario@mariocruz.net>
2023-09-10 07:10:20 -0400

    Update PoemAIGoogleVoice.py
    Add the Signoff with Mario the Maker

--- PoemAIGoogleVoice.py
+++ PoemAIGoogleVoice.py
@@ -1,17 +1,92 @@
-import openai
+import requests
+import cv2
 import os
+import openai
+import platform
 import time
 from google.cloud import texttospeech
-from concurrent.futures import ThreadPoolExecutor
 
-# Set API keys and environment variables
-openai.api_key = os.environ.get('OPENAI_API_KEY')
+# Azure setup
+API_KEY = os.getenv('AZURE_API_KEY')
+ENDPOINT = 'https://mario-image-to-text.cognitiveservices.azure.com/'
+DESCRIBE_URL = f'{ENDPOINT}/vision/v3.1/describe'
+ANALYZE_URL = f'{ENDPOINT}/vision/v3.1/analyze'
+
+headers = {
+    'Ocp-Apim-Subscription-Key': API_KEY,
+    'Content-Type': 'application/octet-stream'
+}
+
+# Initialize OpenAI
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+# Set up Google's Text-to-Speech credentials
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "poem-0-matic-b9f292c4f549.json"
 
 def clear_screen():
-    os.system('cls' if os.name == 'nt' else 'clear')
+    system_name = platform.system().lower()
+    if 'windows' in system_name:
+        os.system('cls')
+    else:
+        os.system('clear')
 
-def speak_text_google(text):
+def capture_image():
+    cam = cv2.VideoCapture(0)
+    if not cam.isOpened():
+        print("Error initializing camera. Please check if it's connected and retry.")
+        return False, None
+
+    time.sleep(2)
+    ret, frame = cam.read()
+    cam.release()
+
+    if ret:
+        cv2.imshow('Captured Image', frame)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    return ret, frame
+
+def analyze_image_with_azure(url, image, params):
+    _, img_encoded = cv2.imencode('.jpg', image)
+    try:
+        response = requests.post(url, headers=headers, data=img_encoded.tobytes(), params=params)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.HTTPError as err:
+        print(f"HTTP error occurred: {err}")
+        print("Error Details:", response.text)
+    except requests.exceptions.RequestException as e:
+        print(f"Error occurred: {e}")
+    return {}
+
+def create_poem(description, dominant_color):
+    context_info = f"The dominant color in the image is {dominant_color}."
+
+    prompt = [
+        {"role": "system", "content": "You are a poetic muse, able to capture the vibrant spirit and allure of Miami with your words."},
+        {"role": "system", "content": "Remember to immerse the reader in Miami's unique atmosphere and culture."},
+        {"role": "system", "content": "in the style of a sonnet"},
+        {"role": "user", "content": f"Write a sonnet based on the image description: {description}. {context_info}"}
+    ]
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=prompt,
+        temperature=0.6
+    )
+
+    choices = response.get('choices', [])
+    if not choices:
+        print("Unexpected response from OpenAI. Couldn't generate poem.")
+        return ""
+
+    return choices[0].get('message', {}).get('content', "")
+
+def speak_text_google(text, pause_before=False):
+    if pause_before:
+        time.sleep(3)
+        
     client = texttospeech.TextToSpeechClient()
     synthesis_input = texttospeech.SynthesisInput(text=text)
     voice = texttospeech.VoiceSelectionParams(
@@ -26,86 +101,48 @@ def speak_text_google(text):
     os.system("afplay output.mp3")  # for MacOS
     os.remove("output.mp3")
 
-def get_user_choice(options, prompt):
-    while True:
-        print(prompt)
-        for i, option in enumerate(options, 1):
-            print(f"{i}. {option}")
-        try:
-            choice = int(input("\nEnter your choice (number): "))
-            if 1 <= choice <= len(options):
-                return options[choice-1]
-            else:
-                print("Invalid choice. Try again.")
-        except ValueError:
-            print("Please enter a valid number.")
-
-def fetch_poem_from_openai(prompt):
-    retries = 3
-    for attempt in range(retries):
-        try:
-            ##response = openai.ChatCompletion.create(model="gpt-4",
-            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", 
-                                                    messages=[{"role": "system", "content": "You are an awesome poet that grew up in miami, florida"},
-                                                              {"role": "user", "content": prompt}])
-            content = response.get('choices', [{}])[0].get('message', {}).get('content')
-            if content:
-                return content.strip()
-            else:
-                return "Error: The response from OpenAI was unexpected."
-        except openai.error.OpenAIError as e:
-            if attempt < retries - 1:
-                time.sleep(1)
-                continue
-            else:
-                return f"OpenAI Error after {retries} attempts: {e}"
-        except Exception as e:
-            return f"Error generating poem: {e}"
-
-def generate_poem_with_openai():
-    colors = ["Red", "Yellow", "Blue", "Orange", "Green"]
-    locations = ["Miami Beach", "Downtown Miami", "Wynwood Miami", "Underline Miami", "Little Havana, Miami"]
-    feelings = ["Happy", "Anxious", "Nostalgic", "Frustrated", "Romantic"]
-    wildcards = ["Mojito", "Music", "Magic", "Epic", "Books"]
-    poem_styles = ["Sonnet", "Haiku", "Free Verse", "Limerick", "Villanelle"]
-
-    while True:
-        color_choice = get_user_choice(colors, "Choose a color:")
-        location_choice = get_user_choice(locations, "Choose a location:")
-        feeling_choice = get_user_choice(feelings, "Choose a feeling:")
-        wildcard_choice = get_user_choice(wildcards, "Choose a wildcard:")
-        poem_style_choice = get_user_choice(poem_styles, "Choose a poem style:")
-
+if __name__ == "__main__":
+    try:
         clear_screen()
 
-        prompt = (f"Write a {poem_style_choice} about {location_choice} with themes of {color_choice}, "
-                 f"{feeling_choice}, and {wildcard_choice}.")
+        if not API_KEY:
+            print("Azure API key not found. Ensure the AZURE_API_KEY environment variable is set.")
+            exit()
 
-        with ThreadPoolExecutor() as executor:
-            future_poem = executor.submit(fetch_poem_from_openai, prompt)
+        if not openai.api_key:
+            print("OpenAI API key not found. Ensure the OPENAI_API_KEY environment variable is set.")
+            exit()
+
+        success, captured_img = capture_image()
+        if not success:
+            print("Failed to grab frame from camera. Check camera availability.")
+            exit()
+
+        # Analyze image for dominant color
+        analysis_params = {
+            'visualFeatures': 'Color'
+        }
+        response_data = analyze_image_with_azure(ANALYZE_URL, captured_img, analysis_params)
+        dominant_color = response_data.get('color', {}).get('dominantColorForeground', "")
+
+        # Get description and tags
+        description_data = analyze_image_with_azure(DESCRIBE_URL, captured_img, {})
+        primary_description = description_data.get('description', {}).get('captions', [{}])[0].get('text', "")
+
+        if primary_description:
+            print("Image Description:", primary_description)
+            poem = create_poem(primary_description, dominant_color)
+            print("\n" + poem)
             
-            # Voice out the user's choices while fetching poem in the background
-            choices = {
-                "Color": color_choice,
-                "Location": location_choice,
-                "Feeling": feeling_choice,
-                "Wildcard": wildcard_choice,
-                "Poem Style": poem_style_choice
-            }
-            for key, value in choices.items():
-                print(f"{key}: {value}")
-                speak_text_google(f"{key}: {value}")
+            # Speak out the poem
+            speak_text_google(poem)
 
-        # Retrieve the generated poem
-        poem = future_poem.result()
-        return poem
-
-clear_screen()
-print("Poem-O-Matic-AI by MarioTheMaker\n")
-poem = generate_poem_with_openai()
-print(poem + "\n")
-speak_text_google(poem)
-time.sleep(2)
-sign_off = "Poem Created by Poem-O-Matic-AI by Mario The Maker  for O'Miami festival"
-print(sign_off)
-speak_text_google(sign_off)
+            sign_off = "Poem Created by Poem-O-Matic-AI by Mario The Maker for O'Miami festival"
+            print(sign_off)
+            
+            # Speak out the sign off after a 3-second pause
+            speak_text_google(sign_off, pause_before=True)
+        else:
+            print("Couldn't retrieve a description for the image.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
